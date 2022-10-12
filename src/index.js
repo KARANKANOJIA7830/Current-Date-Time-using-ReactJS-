1e24a9efd8b7ddfcb3b983ae67793291f7e15a05
Karan Kanojia <86140822+KARANKANOJIA7830@users.noreply.github.com>
2022-10-12 20:17:59 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const Name="Karan Kanojia";
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+let curDate = new Date(2020, 3, 5, 20);
+curDate = curDate.getHours();
+
+let greeting = '';
+
+const cssStyle = {};
+
+if(curDate >= 1 && curDate <12) {
+  greeting = 'good Morning';
+  cssStyle.color = 'green';
+}
+else if (curDate >= 12 && curDate < 16) {
+  greeting = 'good evening';
+  cssStyle.color = 'red';
+}
+else if(curDate >= 16 && curDate <19) {
+  greeting = 'good Evening';
+  cssStyle.color = 'orange';
+}
+else {
+  greeting = 'good Night';
+  cssStyle.color = 'blue';
+}
 
 ReactDOM.render(
   <>
-    <h1>
-      {`Hello, my name is ${Name}`}
-    </h1>
-    <p>Current date is = {currDate} </p>
-    <p>Current time is = {currTime} </p>
+  <div>
+      <h1> Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+  </div>
   </>,
-  document.getElementById("root")
+  document.getElementById('root')  
 );
